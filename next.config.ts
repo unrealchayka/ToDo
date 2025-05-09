3c6b38dd7827271e065bd2ee7f743c0a6696e650
unrealchayka <akorucev@gmail.com>
2025-05-09 14:18:26 +0300

    pwa fixed

--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,19 @@ import type { NextConfig } from "next";
 
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
   runtimeCaching: [
     {
       urlPattern: /^https?.*/,
       handler: 'NetworkFirst',
       options: {
         cacheName: 'offlineCache',
-        expiration: {
-          maxEntries: 200,
-        },
-      },
-    },
-  ],
+        networkTimeoutSeconds: 15
+      }
+    }
+  ]
 });
 const nextConfig: NextConfig = {
   /* config options here */
